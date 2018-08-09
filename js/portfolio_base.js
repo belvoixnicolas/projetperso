63c3e33ce366d41c08dbdc16ec197ba15ec791c2
belvoixnicolas <belvoixnicolas1997@gmail.com>
2018-08-09 16:59:33 +0200

    t

---
+++ js/portfolio_base.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function(){
+    $('#tips, #projet div').hide();
+
+    $('#bouton_projet').click(function() {
+        var projet = document.getElementById('projet').style.display;
+        
+        if (projet == 'none') {
+            $('#tips').animate({opacity: '0'}, 'slow', function () {
+                $('#tips').hide();
+                $('#projet').show();
+                $('#projet').fadeTo(0, 0);
+                $('#projet').animate({opacity: '1'}, 'slow');
+            });
+        }
+    });
+
+    $('#bouton_tips').click(function() {
+        var tips = document.getElementById('tips').style.display;
+        
+        if (tips == 'none') {
+            $('#projet').animate({opacity: '0'}, 'slow', function () {
+                $('#projet').hide();
+                $('#tips').show();
+                $('#tips').fadeTo(0, 0);
+                $('#tips').animate({opacity: '1'}, 'slow');
+            });
+        }
+    });
+
+    $('#projet section').mouseenter(function() {
+        $(this).find("div").show().fadeTo(0, 0).animate({opacity: '1'}, 'slow');
+    });
+    $('#projet section').mouseleave(function() {
+        $(this).find("div").animate({opacity: '0'}, 'slow', function() {
+            $(this).hide();
+        });
+    });
+
+    $('.scroller').click(function() {
+        $('header').animate({height: 'toggle'}, 'slow');
+    });
+
+    $(window).scroll(function() {
+        
+    });
+});
